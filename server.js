9af4f6c0a46ce24d23da14c476608c7c317d328b
Piyush Pushkar <Piyush.Pushkar@mu-sigma.com>
2019-05-20 20:44:39 +0530

    First commit with working chat functionality with text and images

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const socketIo = require('socket.io');
+const app = express();
+const port = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+const io = socketIo(server);
+const users = [];
+let userNumber = 0;
+
+const welcomeMessage =  (userName) => ({message: `Hi! You're connected to the common chat room.`, type: 'text', originalUserName: userName});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const broadcastMessage = (message) => {
+    io.emit('common-room', message);
+};
+
+
+const handleNewConnection = (socket) => {
+    const userName = `user${userNumber++}`;
+    console.log('Sending welcome message', userName);
+    users.push(userName);
+    socket.emit('common-room', welcomeMessage(userName));
+    socket.broadcast.emit('common-room', {message: `${userName} connected!`, type: 'text'});
+    socket.on('common-room', broadcastMessage);
+    socket.on('user-update', (message) => {
+        users[users.indexOf(message.originalUserName)] = message.updatedUserName;
+        console.log('Updated username from ', message.originalUserName, message.updatedUserName, users);
+    });
+    socket.on("disconnect", () => {
+        console.log(`${userName} disconnected`);
+        io.emit('common-room', {message: `${userName} disconnected`, type: 'text', originalUserName: userName});
+    });
+};
+
+io.on('connection', handleNewConnection);
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
---
+++ src/chat-component.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import socketIOClient from 'socket.io-client';
+
+export class ChatComponent extends React.Component {
+    socket;
+    messageView;
+    userName = '';
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+            response: false,
+            endpoint: 'http://localhost:4000',
+            isUsernameSet: false
+        }
+    }
+
+    ab2str(buf) {
+        return String.fromCharCode.apply(null, new Uint16Array(buf));
+    }
+
+    str2ab(str) {
+        var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+        var bufView = new Uint16Array(buf);
+        for (var i = 0, strLen = str.length; i < strLen; i++) {
+            bufView[i] = str.charCodeAt(i);
+        }
+        return buf;
+    }
+
+    componentDidMount() {
+        this.messageView = document.querySelector('#messageView');
+        const { endpoint } = this.state;
+        this.socket = socketIOClient(endpoint);
+        this.socket.on("common-room", data => {
+            if (data.originalUserName) {
+                this.userName = data.originalUserName;
+            }
+            let decodedData = data.message;
+            let currentMessageLine = '';
+            if (data.type === 'image') {
+                const imageBytes = new Uint8Array(data.message);
+                const imageData = 'data:image/png;base64,' + this.encodeTypedArrayToBase64(imageBytes);
+                currentMessageLine = <div><div>{data.userName}:</div><div><img src={imageData} alt="Incoming" width="50%"></img></div></div>;
+            } else {
+                if (data.message instanceof ArrayBuffer) {
+                    decodedData = this.ab2str(data.message);
+                }
+                if (data.userName) {
+                    decodedData = `${data.userName}: ${decodedData}`;
+                }
+                currentMessageLine = <div>{decodedData}</div>;
+            }
+            this.setState({ messages: this.state.messages.concat([currentMessageLine]) });
+        });
+    }
+
+    sendMessage() {
+        const message = document.querySelector('textarea').value;
+        const messageBuffer = this.str2ab(message);
+        this.socket.emit('common-room', { message: messageBuffer, type: 'text', userName: this.userName });
+    }
+
+    sendImage(event) {
+        event.persist();
+        const imageData = event.target.files[0];
+        const fileReader = new FileReader();
+        fileReader.onloadend = (event) => {
+            const imageBuffer = event.target.result;
+            this.socket.emit('common-room', { message: imageBuffer, type: 'image', userName: this.userName });
+        };
+        fileReader.readAsArrayBuffer(imageData);
+    }
+
+    encodeTypedArrayToBase64 (input) {
+        var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+    
+        while (i < input.length) {
+            chr1 = input[i++];
+            chr2 = i < input.length ? input[i++] : Number.NaN; // Not sure if the index 
+            chr3 = i < input.length ? input[i++] : Number.NaN; // checks are needed here
+    
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+    
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+            output += keyStr.charAt(enc1) + keyStr.charAt(enc2) +
+                      keyStr.charAt(enc3) + keyStr.charAt(enc4);
+        }
+        return output;
+    }
+
+    setUsername() {
+        const originalUserName = this.userName;
+        this.userName = document.querySelector('#username').value;
+        this.socket.emit('user-update', {originalUserName: originalUserName, updatedUserName: this.userName});
+        this.setState({isUsernameSet: true});
+    }
+
+    userNameInput() {
+        if (!this.state.isUsernameSet) {
+            return(
+                <div>
+                <input type="text" placeholder="Enter Username" id="username" />
+                <button onClick={(event) => this.setUsername()}>Submit</button>
+                </div>
+        );
+        } else {
+            return(<div></div>);
+        }
+    }
+
+    render() {
+        return (<div id="chat-window-container">
+            <div>
+                Chat
+            </div>
+            <div>
+                {this.userNameInput()}
+            </div>
+            <div id="messageView" style={{height: '600px', overflowY: "auto"}}>
+                {this.state.messages}
+            </div>
+            <div>
+                <textarea placeholder="Enter text here" rows="3"></textarea>
+                <input type="file" onChange={(event) => { this.sendImage(event) }} placeholder="Send Image" />
+            </div>
+            <div>
+                <button onClick={() => { this.sendMessage() }}>Send</button>
+            </div>
+        </div>);
+    }
+}
+
