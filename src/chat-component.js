efbba90f9fef524b19278f4e4907e8450261cd2e
techluv13 <piyush.mvit13@gmail.com>
2019-05-20 22:50:17 +0530

    Corrected socket io port

--- src/chat-component.js
+++ src/chat-component.js
@@ -10,7 +10,7 @@ export class ChatComponent extends React.Component {
         this.state = {
             messages: [],
             response: false,
-            endpoint: `${window.location.protocol}//${window.location.hostname}:4000`,
+            endpoint: `${window.location.protocol}//${window.location.hostname}`,
             isUsernameSet: false
         }
     }
